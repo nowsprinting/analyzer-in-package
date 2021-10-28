09a26235557696bb0de8918c4dcf4da2e01dbfef
Koji Hasegawa <hasegawa@hubsys.co.jp>
2021-10-29 06:46:06 +0900

    Rename asmdef

--- WithAsmdef/Dummy.cs
+++ WithAsmdef/Dummy.cs
@@ -1,18 +1,7 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Dummy : MonoBehaviour
+namespace AnalyzerInPackageAsm
 {
-    // Start is called before the first frame update
-    void Start()
+    // ReSharper disable once InconsistentNaming
+    public class DUMMY
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
